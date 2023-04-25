ee609464f576f28e790609d2bd915d58bdb2abb9
Vincent Vandyck <vincent.vandyck@gmail.com>
2023-04-25 17:07:53 +0000

    removed send command read, added m1, m2 position functions

--- PyRoboteq/roboteq_handler.py
+++ PyRoboteq/roboteq_handler.py
@@ -1,5 +1,6 @@
 import serial
 import time
+import roboteq_ros.PyRoboteq.roboteq_21_commands as cmd
 # message
 class RoboteqHandler:
     """
@@ -52,7 +53,7 @@ class RoboteqHandler:
             
         return self.is_alive
 
-    def send_raw_command(self, str_command: str = "") -> None:
+    def send_raw_string(self, str_command: str = "") -> None:
         """
         Send a raw string command, the library will handle sending the command, but how you write it
         is up to you.
@@ -71,6 +72,7 @@ class RoboteqHandler:
             if self.exit_on_interrupt:
                 quit()
         
+
     def request_handler(self, request: str = "") -> str:
         """
         Sends a command and a parameter, 
@@ -93,7 +95,7 @@ class RoboteqHandler:
                 print(f"DEBUG MODE: Rx:{raw_data}")
             return raw_data
 
-        self.send_raw_command(request)
+        self.send_raw_string(request)
         result = get_data(self.ser)
         result = result.decode()
         result = result.split("\r")
@@ -108,27 +110,17 @@ class RoboteqHandler:
                 print(debug_return)
             return debug_return
 
-    def dual_motor_control(self, left_motor: int = 0, right_motor: int = 0) -> None:
-        """
-        Controlling the motor using a Dual Drive mode
-        Send speed for the left, and right side of the robot/vehicle seperately 
-        Effective for doing Pivot drive and running track based robots
-        left_motor: integer from -1000 to 1000
-        right_motor: integer from -1000 to 1000
-        """
-        raw_command = f"!M {left_motor} {right_motor} "
-        self.request_handler(raw_command)
-    
+
     def send_command(self, command: str, first_parameter = "", second_parameter = "") -> None:
-        message = f"{command} {first_parameter} {second_parameter}".strip()
+        message = f"{command} {first_parameter} {second_parameter}"
 
         try:
-            response = self.request_handler(message)
+            self.send_raw_string(message)
         except Exception as e:
             if self.debug_mode:
                 print("DEBUG MODE: Failed to construct a message, read the exception error below:")
                 print(e)
-                print(f"Received message: {response}")
+                print(f"Received exception: {e}")
                 print("\n")
             if self.exit_on_interrupt:
                 quit()
@@ -143,3 +135,19 @@ class RoboteqHandler:
         request = f"{command} [{parameter}]"
         response = self.request_handler(request)
         return response
+
+    def set_motor1_position(self, position: int) -> None:
+        """
+        Set the position of motor 1.
+        :param position: Absolute count destination for motor 1, signed 32-bit integer from -2147M to +2147M.
+        """
+        command = f"{cmd.GO_TO_ABS_POS} 1 {position}"
+        self.send_command_request(command)
+
+    def set_motor2_position(self, position: int) -> None:
+        """
+        Set the position of motor 2.
+        :param position: Absolute count destination for motor 2, signed 32-bit integer from -2147M to +2147M.
+        """
+        command = f"{cmd.GO_TO_ABS_POS} 2 {position}"
+        self.send_command_request(command)
